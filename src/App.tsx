c0133582c00f64d315d932b087cffaf8805cae6c
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-02-19 14:40:06 -0800

    almost done with input field 2

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,11 @@ const App: React.FC = () => {
 
   //state for 1 to do inside input field
   const [todo, setToDo] = useState<string>("")
+  const [todo2, setToDo2] = useState<string>("")
   //this is how you create a type of an array or interface
   //state for all of our toDos
   const [toDoList, setToDoList] = useState<Todo[]>([])
+  const [toDoList2, setToDoList2] = useState<Todo[]>([])
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault()
@@ -27,6 +29,14 @@ const App: React.FC = () => {
   }
   };
 
+  const handleAdd2 = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (todo2) {
+      setToDoList2([...toDoList2, {id: Date.now(), todo, isDone: false}])
+      setToDo2("")
+    }
+  }
+
   console.log(toDoList)
   return (
     <Box className="App">
@@ -35,7 +45,7 @@ const App: React.FC = () => {
       {/* {toDoList.map((element) => (<li>{element.todo}</li>))} */}
       <ToDoList toDoList={toDoList} setToDoList={setToDoList}/>
       {/* <ToDoList /> */}
-      <InputField2 />
+      <InputField2 todo={todo2} setToDo={setToDo2} handleAdd2={handleAdd2} />
       
     </Box>
   );
--- src/components/InputField2.tsx
+++ src/components/InputField2.tsx
@@ -1,15 +1,25 @@
 import { FormControl, OutlinedInput, InputLabel, Button} from '@mui/material';
 
-const InputField2 = () => {
+interface Props {
+    todo: string
+    setToDo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd2: (e: React.FormEvent) => void
+}
+
+export const InputField2 = ({todo, setToDo, handleAdd2}: Props) => {
   return (
     <>
     <FormControl margin='normal' sx={{marginTop: '100px'}}>
-        <InputLabel></InputLabel>
-        <OutlinedInput 
-        
+        <InputLabel htmlFor="task">Task</InputLabel>
+        <OutlinedInput
+        id="task" 
+        value={todo}
+        label="Task"
+        onChange={(e) => setToDo(e.target.value)}
         />
     </FormControl>
-    <Button variant='contained'>Go</Button>
+    <Button 
+    variant='contained' onClick={handleAdd2}>Go</Button>
     </>
   )
 }
@@ -20,4 +30,3 @@ export default InputField2
 
 
 
-export {}
